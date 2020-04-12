b64060b479b21eda359e6c05a50148dc7b72d25f
koniman <user@USER-no-MacBook-Pro.local>
2020-04-12 23:02:47 +0900

    kannsei

--- old.rb
+++ old.rb
@@ -1,33 +1,39 @@
 require"./rrr"
-
 menu1 = Menu.new(name:"沖縄旅行",price:10000)
 menu2 = Menu.new(name:"北海道旅行",price:20000)
 menu3 = Menu.new(name:"九州旅行",price:15000)
 
-
 menus=[menu1,menu2,menu3]
+index=1
+menus.each do |menu|
+  puts "#{index}.#{menu.info}"
+  index +=1
 
-menus.each.with_index(1) do |menu,i|
-  puts "#{i}.#{menu.info}"
 end
 
 puts "プランを選択してください"
-order = 0
-loop do
-order= gets.chomp.to_i
-break if (1..3).include?(order)
 
-  puts "1~3番からお選びください"
-end
+order=gets.chomp.to_i
 
-selected_menu = menus[order -1]
-puts "#{selected_menu.name}ですね、何人でいきますか？"
+selected_menu = menus[order]
+
+if order == 1 
+  puts "#{menu1.name}ですね、何人でいきますか？"
+elsif order == 2
+  puts "#{menu2.name}ですね、何人でいきますか？"
+  elsif order == 3
+    puts "#{menu3.name}ですね、何人でいきますか？"
+  else 
+    puts "1~3で入力してください"
+  end
+  
 
 count = gets.chomp.to_i
 if count< 5
   puts "合計料金:¥ #{selected_menu.get_total_price(count)}"
-else
+elsif count >=5
   puts "5人以上なので10％引きになります"
   puts "合計料金:¥ #{selected_menu.get_total_price(count)}"
-
+else 
+  puts "error"
 end
